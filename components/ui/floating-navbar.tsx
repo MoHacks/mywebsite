3b5226789f07fd5e176a5e8aa6b0016be3915bb8
MoHacks <mohamad_alkhani@hotmail.com>
2024-06-23 16:58:55 -0400

    update navbar

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -22,12 +22,23 @@ export const FloatingNav = ({
 }) => {
   const { scrollYProgress } = useScroll();
 
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true); //navbar starts off visible
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
     if (typeof current === "number") {
-      let direction = current - scrollYProgress.getPrevious()!;
-      setVisible(scrollYProgress.get() > 0.05 && direction < 0);
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        // also set true for the initial state
+        setVisible(true);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
     }
   });
 
@@ -46,7 +57,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-7 items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border dark:bg-custom-dark dark:bg-[#5c2d2d66] dark:border-purple/[0.8] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-7 items-center justify-center space-x-4",
           className
         )}
       >
@@ -59,10 +70,10 @@ export const FloatingNav = ({
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm relative">
+            {/* <span className="hidden sm:block text-sm relative"> */}
               {navItem.name}
               <span className="absolute inset-x-0 bottom-0 h-px bg-gradient-to-r from-transparent to-transparent group-hover:via-red-500 opacity-0 group-hover:opacity-100 transition-opacity" />
-            </span>
+            {/* </span> */}
           </Link>
         ))}
       </motion.div>
