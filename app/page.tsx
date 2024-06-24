4aeb10c214386207b7d2e5cbc8cb34a6dbe207b7
MoHacks <mohamad_alkhani@hotmail.com>
2024-06-24 08:05:53 -0400

    revert

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from '../data/index';
-
+// import MyProjects from '../components/MyProjects';
+import React from "react";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
         <FloatingNav navItems={navItems} />
         <Hero />
+        {/* <MyProjects /> */}
         <Grid />
         <h1>Hello, Portfolio!</h1>
       </div>
