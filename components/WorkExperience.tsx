195ef1174ba0a690974226e34d4ff07f94417564
mohamad <mohamad_alkhani@hotmail.com>
2024-06-29 15:23:23 -0400

    unnessary comment

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,33 +1,3 @@
-// import Image from "next/image"; // i want this removed
-// // import combinedimage3 from "../public/combinedimage3.png";
-// import { ContainerScroll } from "./ui/container-scroll-animation";
-// export const WorkExperience = () => {
-//   return (
-//     <div className="flex flex-col pt-20 mt-20" id="experience">
-//       <ContainerScroll
-//         titleComponent={
-//           <h1 className="text-4xl font-semibold text-black dark:text-purple">
-//             <span className="text-4xl md:text-[3rem] font-bold mt-1 leading-none">
-//               Work Experience
-//             </span>
-//           </h1>
-//         }
-//       >
-//         <Image
-//           src={'/combinedimage3.png'} unoptimized // Adjusted the src prop
-//           alt="linear"
-//           height={500} // Adjusted to numeric value
-//           width={800} // Adjusted to numeric value
-//           className="mx-auto rounded-2xl object-cover h-full object-left-top"
-//           draggable={false}
-//         />
-//       </ContainerScroll>
-//     </div>
-//   );
-// };
-
-// export default WorkExperience;
-
 
 import combinedimage3 from "../public/combinedimage3.png";
 import { ContainerScroll } from "./ui/container-scroll-animation";
